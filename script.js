38104b8ad830082f9ce9332dda060a9cc3814257
Prashid Pal <67044025+Prashid1098@users.noreply.github.com>
2023-07-27 20:30:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	if(num==0)
+	{
+		return 0;
+	}
+	if(num==1 || num==2)
+	{
+		return 1;
+	}
+	let a=0,b=1,c=0;
+	for(let i=3;i<=num;i++)
+		{
+			c=a+b;
+			c=a;
+			a=b;
+		}
+	return c;
 }
 
 module.exports = fibonacci;
